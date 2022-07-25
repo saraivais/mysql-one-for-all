7b8e496f680869e5081516a8e353f8fbb9db31c0
saraivais <isadorasaraiva95@gmail.com>
2022-07-25 13:34:15 -0300

    refactor: song, album & artist count using join~

--- desafio2.sql
+++ desafio2.sql
@@ -1,8 +1,10 @@
-SELECT
-	COUNT(DISTINCT so.song_id) AS 'cancoes',
-	COUNT(DISTINCT ar.artist_id) AS 'artistas',
-  COUNT(DISTINCT al.album_id) AS 'albuns'
-FROM
-	SpotifyClone.artists AS ar,
-  SpotifyClone.albums AS al,
-  SpotifyClone.songs AS so;
+SELECT 
+	COUNT(DISTINCT so.song_id) AS cancoes,
+    COUNT(DISTINCT art.artist_id) AS artistas,
+	COUNT(DISTINCT alb.album_id) AS albuns
+FROM 
+	SpotifyClone.songs AS so
+    INNER JOIN SpotifyClone.albums AS alb
+		ON so.album_id = alb.album_id
+    INNER JOIN SpotifyClone.artists AS art
+		ON alb.artist_id = art.artist_id;
